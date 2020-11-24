0218b1d025f1d5344e39f606e48ac8b6c40b2deb
jperozog <perozo64@gmail.com>
2020-11-24 13:03:44 -0400

    Updates

---
+++ static/js/53.7b9edab3.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpmilugarmarket=this.webpackJsonpmilugarmarket||[]).push([[53],{136:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return o}));var i=e(13),o=function(){var n=window;n.addEventListener("statusTap",(function(){Object(i.h)((function(){var t=n.innerWidth,e=n.innerHeight,o=document.elementFromPoint(t/2,e/2);if(o){var r=o.closest("ion-content");r&&r.componentOnReady().then((function(){Object(i.f)((function(){return r.scrollToTop(300)}))}))}}))}))}}}]);
+//# sourceMappingURL=53.7b9edab3.chunk.js.map
